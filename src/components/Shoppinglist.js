099bc39e635b59389663dd984045a2fe5a241d56
Joris Bertier <joris.bertier@gmail.com>
2024-01-03 17:02:59 +0100

    add plant & categorie

--- src/components/Shoppinglist.js
+++ src/components/Shoppinglist.js
@@ -18,15 +18,27 @@
 
 import { plantList } from '../datas/plantList'
 
-const results = [];
-
 function ShoppingList() {
-	return <ul>{plantList.forEach((plant, index) => (
-        results.push(<li key={`${plant}-${index}`}>{plant}</li>)
-    )
-    )
-    }
-        </ul>
+    const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
+
+	return (
+		<div>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul>
+				{plantList.map((plant) => (
+					<li key={plant.id}>{plant.name}</li>
+				))}
+			</ul>
+		</div>
+	)
 }
 
 export default ShoppingList
